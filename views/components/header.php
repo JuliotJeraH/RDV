4a707af1c28552491375ffe0c9c3733e64777410
JuliotJeraH <juliotjera7@gmail.com>
2025-07-15 05:50:50 +0200

    style sisa

--- views/components/header.php
+++ views/components/header.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Application Médicale</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/public/css/style.css" rel="stylesheet">
+    <link href="public/css/style.css" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <a class="navbar-brand" href="">App Médicale</a>
+            <a class="navbar-brand title" href="">RDV MEDICAL</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
